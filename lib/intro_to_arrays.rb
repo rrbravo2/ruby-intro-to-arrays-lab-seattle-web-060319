288bcfc0fea03e603de9ebdcac97605cf5066ff6
Ryan Bravo <rrbravo2@gmail.com>
2019-05-21 06:04:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,7 @@ end
 def last_element(my_last_element)
  my_last_element = @taylor_swift[-1]
 end
+
+def first_element_with_array_methods(first_country)
+ first_country = @south_east_asia.first
+end
